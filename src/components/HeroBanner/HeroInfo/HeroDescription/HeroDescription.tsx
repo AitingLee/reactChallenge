2516a7cff311047bab34966c74414520bee01937
AitingLee <aiting.lee@viewsonic.com>
2023-12-09 12:54:12 +0800

    feat: implement demo button click handler

--- src/components/HeroBanner/HeroInfo/HeroDescription/HeroDescription.tsx
+++ src/components/HeroBanner/HeroInfo/HeroDescription/HeroDescription.tsx
@@ -1,21 +1,29 @@
 import { HeroDescriptionDemoButton, HeroDescriptionContentDiv, HeroDescriptionDiv, HeroDescriptionTitleDiv } from "@/components/HeroBanner/HeroInfo/HeroDescription/styles";
 
-
+const demoLink = "https://universe.viewsonic.io/demo";
 
 function HeroDescription() {
+
+    const handleDemoButtonClick = () => {
+        window.open(demoLink, '_blank');
+    };
+
     return (
         <HeroDescriptionDiv>
-            <HeroDescriptionTitleDiv>Your Virtual Campus for Online Learning</HeroDescriptionTitleDiv>
+            <HeroDescriptionTitleDiv>
+                Your Virtual Campus for Online Learning
+            </HeroDescriptionTitleDiv>
             <HeroDescriptionContentDiv>
                 Bring the benefits of in-person learning to an online environment.
             </HeroDescriptionContentDiv>
             <HeroDescriptionContentDiv>
                 Explore virtual learning spaces, engage with the class through customizable avatars and collaborate online as if it were in-person.
             </HeroDescriptionContentDiv>
-            <HeroDescriptionDemoButton>SCHEDULE A DEMO</HeroDescriptionDemoButton>
+            <HeroDescriptionDemoButton onClick={handleDemoButtonClick}>
+                SCHEDULE A DEMO
+            </HeroDescriptionDemoButton>
         </HeroDescriptionDiv>
     )
 }
-
 
 export default HeroDescription;
